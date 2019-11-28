122d2c4f0a5a220efbdf7ce3b63e92c69f778130
Freddy <Freddypimpns@gmail.com>
2019-11-28 02:10:34 -0400

    Se agrego el fomulario para el registro de empresa

--- init.py
+++ init.py
@@ -1,15 +1,19 @@
-from flask import Flask, render_template, request, Request
+from flask import Flask, render_template, request, Request, jsonify
 import con
+import json
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/create", methods = ['POST'] )
+@app.route("/create/", methods = ['POST'] )
 def create_db():
-    nombre = request.form.get("nombre")
-    #print(nombre)
+    nombre = request.get_json("datos")
+    valor = nombre['nom']
+    print(nombre['nom'])
+    return jsonify(nombre)
+   
 if __name__ == '__main__':
     app.run(debug=True)
     
--- model.py
+++ model.py
@@ -6,7 +6,7 @@ def create_bussines(nombre_empresa):
         data.execute(f" create database {nombre_empresa};")
         print (f"base de datos creada con el nombre {nombre_empresa}")
         data.execute(f"use {nombre_empresa};")
-        data.execute(f"CREATE table {nombre_empresa} (id_empresa int primary key auto_increment, nombre varchar (100), rnc varchar (50), direccion varchar (300), telefono varchar (50), correo varchar (50), encargado varchar (100), logo varchar (200), fecha_registro timestamp);")
+        data.execute(f"CREATE table {nombre_empresa} (id_empresa int primary key auto_increment, nombre varchar (100), rnc varchar (50), provincia varchar (100), direccion varchar (300), telefono varchar (50), correo varchar (50), encargado varchar (100), logo varchar (200), fecha_registro timestamp);")
         data.execute(f"CREATE table users (id_user int primary key auto_increment, nombre varchar (100), password varchar (50), rol varchar (30));")
         print ("Tablas de registro de la empresa creada")
     except:
@@ -17,7 +17,7 @@ def registrar(nombre, rnc, direccion, telefono, correo, encargado):
     try:
         data = con.conexion.cursor()
         data.execute(f"use {nombre}")
-        data.execute(f"INSERT INTO `{nombre}` (`id_empresa`, `nombre`, `rnc`, `direccion`, `telefono`, `correo`, `encargado`) VALUES (NULL, '{nombre}', '{rnc}', '{direccion}', '{telefono}', '{correo}', '{encargado}');")
+        data.execute(f"INSERT INTO `{nombre}` (`id_empresa`, `nombre`, `rnc`,`provincia`, `direccion`, `telefono`, `correo`, `encargado`) VALUES (NULL, '{nombre}', '{rnc}', '{provincia}', '{direccion}', '{telefono}', '{correo}', '{encargado}');")
         con.conexion.commit()
         print('registo de la empresa')
     except ():
@@ -34,5 +34,5 @@ def create_user(db, nombre, password):
         print("no funciono la insert")
 
 print (create_bussines('SoftFlesh'))
-print(registrar('SoftFlesh','RDF22222152', 'Calle SAntiago Rodriguez', '809-536-4833', 'Freddypimpns@gmail.com', 'Freddy Pimpns'))
+print(registrar('SoftFlesh','RDF22222152','Santo Domingo', 'Calle SAntiago Rodriguez', '809-536-4833', 'Freddypimpns@gmail.com', 'Freddy Pimpns'))
 print(create_user('SoftFlesh','Freddy','Freddy0770'))
